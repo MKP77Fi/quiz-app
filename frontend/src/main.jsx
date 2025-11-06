41a5e22ed862f2831c39ce8f1431f59ee3795c58
MKP77Fi <rekrytointi.tswgroup@gmail.com>
2025-11-06 15:13:06 +0200

    CSS rakennettu

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+
+// Globaalit tyylit
+import './styles/global.css'
+import './styles/components/buttons.css'
+import './styles/components/cards.css'
+import './styles/components/forms.css'
+import './styles/components/header.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>

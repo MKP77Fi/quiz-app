62d774a024ac9bc5817a99b913d841d8dddbfe09
MKP77Fi <rekrytointi.tswgroup@gmail.com>
2025-11-20 13:12:02 +0200

    PracticeView korjattu

--- frontend/src/components/PracticeView.jsx
+++ frontend/src/components/PracticeView.jsx
@@ -20,7 +20,7 @@ function PracticeView() {
   const navigate = useNavigate(); // ✅ navigointi käyttöön
 
   useEffect(() => {
-    fetch("${import.meta.env.VITE_API_URL}/questions")
+    fetch(`${import.meta.env.VITE_API_URL}/questions`)
       .then((res) => res.json())
       .then((data) => {
         const randomized = shuffleArray(data).map((q) => ({
